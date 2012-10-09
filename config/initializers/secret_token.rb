40009b386f63407c769534497430cec8c9d36cb6
Christine Murabito <christine.murabito@gmail.com>
2012-10-09 10:37:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExploreTheWorld::Application.config.secret_token = '34fefd5ad9cdf7dd2de170b193b77e2436e7353b1e1c481d6a63bea82246ef3ad77a3da3918ec33ac2f8b7d998591fc9c24f6b8163965d151c5603e55b5b63d7'
